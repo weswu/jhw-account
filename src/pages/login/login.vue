dee7fdbefa271b11977c1d14ad125210c5e7f91d
weswu <836790625@qq.com>
2018-02-28 16:55:49 +0800

    wxw

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,280 +1,5 @@
-<style lang="less">
-  body {
-    background: #fff;
-    margin: 0;
-  }
-  .header{
-    height: 85px;
-    .wapper{
-      width: 1000px;
-      margin: 0 auto;
-      img{
-        margin-top: 15px
-      }
-    }
-  }
-  .content{
-    background: #202020;
-    .wapper{
-      height: 600px;
-      width: 1000px;
-      margin: 0 auto;
-      background: url(http://www.jihui88.com/member/static/images/bg.png) no-repeat 20px 85px;
-    }
-    // 表单
-    .form{
-      width: 351px;
-      height: 501px;
-      box-sizing: border-box;
-      border: 1px solid #b9b9b9;
-      border-radius: 5px;
-      background: #fff;
-      float: right;
-      margin-top: 28px;
-      text-align: center;
-      font-family: '微软雅黑';
-      position: relative;
-      .logo{
-        margin: 82px auto 28px auto;
-      }
-      .f-btn {
-        display: block;
-        margin: 8px auto;
-        width: 257px;
-        height: 40px;
-        line-height: 40px;
-        border: 1px solid #ff7333;
-        border-radius: 40px;
-        color: #ff7333;
-        font-size: 14px;
-        text-decoration: none;
-      }
-      .other{
-        padding-top: 80px;
-      }
-      .other-text{
-        text-align: center;
-        line-height: 30px;
-        height: 30px;
-        clear: both;position: relative;
-        &:after {
-          content: "";
-          background: url(http://www.jihui88.com/member/static/images/f-border.png) no-repeat;
-          width: 85px;
-          height: 3px;
-          margin: 14px 0 0 -175px;
-          display: inline-block;
-          position: absolute;
-        }
-        &:before {
-          content: "";
-          background: url(http://www.jihui88.com/member/static/images/f-border.png) no-repeat 0 -3px;
-          width: 85px;
-          height: 3px;
-          margin: 14px 0 0 90px;
-          display: inline-block;
-          position: absolute;
-        }
-        span {
-          width: 90px;
-          display: inline-block;
-          background: #fff;
-          color: #5b5b5b;
-          font-size: 12px;
-          box-sizing: border-box;
-        }
-      }
-      .other-icon{
-        padding-top: 5px;
-        span{
-          display: inline-block;
-          width: 50px;
-          color: #9b9b9b;
-          font-size: 12px;
-          margin: 0 5px;
-        }
-      }
-      .f-icon{
-        width: 39px;
-        height: 39px;
-        display: inline-block;
-        margin: 0 8px;
-        background: url(http://www.jihui88.com/member/static/images/f-icon.png) no-repeat;
-      }
-      .icon-wx{
-        background-position: -39px 0;
-      }
-      .icon-mobile{
-        background-position: -78px 0;
-      }
-    }
-
-    .back-other{
-      color: #666666;
-      font-size: 12px;
-      position: absolute;
-      bottom: 17px;
-      width: 100px;
-      margin-left: -49px;
-    }
-    .back-other:hover{
-      text-decoration: underline;
-    }
-  }
-  .close{
-    position: absolute;
-    padding: 2px;
-    right:11px;
-    top:13px;
-    cursor: pointer;
-  }
-  // 登录
-  .f-login{
-    width: 260px;
-    margin: 0 auto;
-    .f-checkbox{
-      padding: 10px 0;
-      text-align: left;
-      font-size: 12px;
-      color: #ccc;
-    }
-    .veriImg{
-      position: absolute;
-      right: 15px;
-      margin-top: -29px;
-    }
-    .f-checkout-select{
-      width: 10px;height: 10px;display: inline-block; margin-right: 5px;
-      background: url(http://www.jihui88.com/member/static/images/checkbox-selected.png) no-repeat;
-    }
-    .f-checkout-select.select{
-      background-position: -10px 0;
-    }
-  }
-  // 表单
-  .f-input{
-    width: 260px;
-    box-sizing: border-box;
-    border: 1px solid #d9d9d9;
-    border-radius: 5px;
-    margin: 0 auto;
-    position: relative;
-    input{
-      border-radius: 5px;
-      border:none;
-      width:228px;
-      height: 17px;
-      padding: 10px 15px;
-      outline: 0 none;
-    }
-    .username{
-      border-bottom: 1px solid #d9d9d9;
-      border-bottom-right-radius: 0;
-      border-bottom-left-radius: 0;
-    }
-    .forget-password{
-      position: absolute;
-      color: #ccc;
-      font-size: 12px;
-      right: 15px;
-      margin-top: -25px;
-    }
-  }
-  .submit{
-    background: #ff6700;
-    border-radius: 5px;
-    border: 0 none;
-    cursor: pointer;
-    display: block;
-    width: 260px;
-    margin: 0 auto;
-    text-align: center;
-    height: 38px;
-    color: #fff;
-    font-size: 14px;
-    transition: all 0.3s ease 0s;
-    -moz-transition: all 0.3s ease 0s;
-    -webkit-transition: all 0.3s ease 0s;
-    -o-transition: all 0.3s ease 0s;
-  }
-  .submit:hover {
-  	background:#ff4800;
-  }
-
-  .f-register{
-    .submit{
-      margin-top: 15px;
-    }
-  }
-  .f-message{
-    text-align: left;
-    width: 260px;
-    margin: 0 auto;
-    .tip{
-      font-size: 12px;
-      color: #666;
-      text-align: center;
-      padding-bottom: 10px;
-    }
-    input{
-      border: 1px solid #d9d9d9;
-      padding: 10px 15px;
-      width: 90px;
-      outline: 0 none;
-      margin-top: 10px;
-      border-radius: 3px;
-    }
-    img{
-      vertical-align: middle;
-    }
-    .mobileCode{
-      width: 127px;
-      background: #dedede;
-      color: #999;
-      cursor: pointer;height: 36px;
-    float: right;
-    }
-    .submit{
-      margin-top: 15px;
-    }
-  }
-  .alert {
-    width: 800px;
-    height: 600px;
-    margin: -300px 0 0 -400px;
-    position: fixed;
-    left: 50%;
-    top: 50%;
-    background: #fafafa;
-    z-index: 999;
-    border-radius: 3px;
-    box-shadow: 1px 1px 3px #aaa;
-    .head{
-      text-align: center;
-      font-size: 12px;
-      height: 39px;
-      line-height: 39px;
-      border-top-left-radius: 3px;
-      border-top-right-radius: 3px;
-      background: linear-gradient(#fff, #ddd);
-    }
-    .container{
-      width: 786px;
-      height: 554px;
-      margin: 1px auto 0 auto;
-      border: 2px solid #a5a5a5;
-      background: #fff;
-      #wxlogin_container{
-        text-align: center;
-      }
-      iframe{
-        width: 100%;height: 554px;border: none;
-      }
-    }
-  }
-</style>
 <template>
-  <div>
+  <div :class="[]">
     <div class="header">
       <div class="wapper">
         <img src="http://www.jihui88.com/member/static/images/logo2.jpg" alt="">
@@ -305,15 +30,21 @@
             <div class="f-input">
               <input class="username" name="username" v-model="model.username" @keyup.enter="submit" type="text" placeholder="手机号／账号"/>
               <input class="username" name="subusername" v-model="model.subusername" @keyup.enter="submit" type="text" placeholder="请输入员工账号" v-if="checked"/>
-              <input class="username" type="text" name="randCode" v-model="model.randCode" @keyup.enter="submit" placeholder="填写验证码">
-              <img class="veriImg" :src="'http://www.jihui88.com/veriImg'+verifyPic"  @click="refreshCode"/><br/>
-              <input class="password" name="password" v-model="model.password" @keyup.enter="submit" type="password" placeholder="请输入密码"/>
-              <a class="forget-password" target="_blank" href="http://www.jihui88.com/member/forget_password.html">重设密码</a>
+              <div class="">
+                <input class="password username" name="password" v-model="model.password" @keyup.enter="submit" type="password" placeholder="请输入密码"/>
+                <a class="forget-password" target="_blank" href="http://www.jihui88.com/member/forget_password.html">重设密码</a>
+              </div>
+              <div class="">
+                <input type="text" name="randCode" v-model="model.randCode" @keyup.enter="submit" placeholder="填写验证码">
+                <img class="veriImg" :src="'http://www.jihui88.com/veriImg'+verifyPic"  @click="refreshCode"/>
+              </div>
             </div>
             <div class="f-checkbox">
-              <span class="f-checkout-select" v-if="!checked"></span>
-              <span class="f-checkout-select select" v-if="checked"></span>
-              <input type="checkbox" v-model="checked" hidden><label @click="check">使用员工账号登录</label>
+              <input type="checkbox" v-model="checked" hidden>
+              <label @click="check">
+                <span class="f-checkout-select" v-if="!checked"></span>
+                <span class="f-checkout-select select" v-if="checked"></span>使用员工账号登录
+              </label>
             </div>
             <button type="button" class="submit" @click="submit">登录</button>
           </div>
@@ -352,11 +83,9 @@
         <iframe v-if="page === 'qq'" :src="qqUrl"></iframe>
       </div>
     </div>
-    <UserBind :openid="openid" :type="type" :oauthtype="oauthtype"  :redirectURL="model.redirectURL"  :backURL="backURL" v-if="openid !== ''"></UserBind>
   </div>
 </template>
 <script>
-import UserBind from './userBind.vue'
 export default {
   data　() {
     return {
@@ -388,11 +117,9 @@ export default {
         password: ''
       },
       // qq
-      qqUrl: ''
+      qqUrl: '',
+      isMobile: false
     }
-  },
-  components: {
-    UserBind
   },
   created () {
     this.openid = this.getUrlParam('openid') || ''
@@ -408,6 +135,8 @@ export default {
     this.addBind = this.getUrlParam('addBind')
     // 绑定类型
     this.bindType = this.getUrlParam('bindType')
+
+    window.ua = navigator.userAgent.toLowerCase()
   },
   mounted: function () {
     this.$nextTick(function () {
@@ -644,3 +373,280 @@ export default {
   }
 }
 </script>
+<style lang="less">
+  body {
+    background: #fff;
+    margin: 0;
+  }
+  a {
+    text-decoration: none;
+  }
+  .header{
+    height: 85px;
+    .wapper{
+      width: 1000px;
+      margin: 0 auto;
+      img{
+        margin-top: 15px
+      }
+    }
+  }
+  .content{
+    background: #202020;
+    .wapper{
+      height: 600px;
+      width: 1000px;
+      margin: 0 auto;
+      background: url(http://www.jihui88.com/member/static/images/bg.png) no-repeat 20px 85px;
+    }
+    // 表单
+    .form{
+      width: 351px;
+      height: 501px;
+      box-sizing: border-box;
+      border: 1px solid #b9b9b9;
+      border-radius: 5px;
+      background: #fff;
+      float: right;
+      margin-top: 28px;
+      text-align: center;
+      font-family: '微软雅黑';
+      position: relative;
+      .logo{
+        margin: 82px auto 28px auto;
+      }
+      .f-btn {
+        display: block;
+        margin: 8px auto;
+        width: 257px;
+        height: 40px;
+        line-height: 40px;
+        border: 1px solid #ff7333;
+        border-radius: 40px;
+        color: #ff7333;
+        font-size: 14px;
+      }
+      .other{
+        padding-top: 80px;
+      }
+      .other-text{
+        text-align: center;
+        line-height: 30px;
+        height: 30px;
+        clear: both;position: relative;
+        &:after {
+          content: "";
+          background: url(http://www.jihui88.com/member/static/images/f-border.png) no-repeat;
+          width: 85px;
+          height: 3px;
+          margin: 14px 0 0 -175px;
+          display: inline-block;
+          position: absolute;
+        }
+        &:before {
+          content: "";
+          background: url(http://www.jihui88.com/member/static/images/f-border.png) no-repeat 0 -3px;
+          width: 85px;
+          height: 3px;
+          margin: 14px 0 0 90px;
+          display: inline-block;
+          position: absolute;
+        }
+        span {
+          width: 90px;
+          display: inline-block;
+          background: #fff;
+          color: #5b5b5b;
+          font-size: 12px;
+          box-sizing: border-box;
+        }
+      }
+      .other-icon{
+        padding-top: 5px;
+        span{
+          display: inline-block;
+          width: 50px;
+          color: #9b9b9b;
+          font-size: 12px;
+          margin: 0 5px;
+        }
+      }
+      .f-icon{
+        width: 39px;
+        height: 39px;
+        display: inline-block;
+        margin: 0 8px;
+        background: url(http://www.jihui88.com/member/static/images/f-icon.png) no-repeat;
+      }
+      .icon-wx{
+        background-position: -39px 0;
+      }
+      .icon-mobile{
+        background-position: -78px 0;
+      }
+    }
+
+    .back-other{
+      color: #666666;
+      font-size: 12px;
+      position: absolute;
+      bottom: 17px;
+      width: 100px;
+      margin-left: -49px;
+    }
+    .back-other:hover{
+      text-decoration: underline;
+    }
+  }
+  .close{
+    position: absolute;
+    padding: 2px;
+    right:11px;
+    top:13px;
+    cursor: pointer;
+  }
+  // 登录
+  .f-login{
+    width: 260px;
+    margin: 0 auto;
+    .f-checkbox{
+      padding: 10px 0;
+      text-align: left;
+      font-size: 12px;
+      color: #ccc;
+    }
+    .veriImg{
+      position: absolute;
+      right: 15px;
+      margin-top: -29px;
+    }
+    .f-checkout-select{
+      width: 10px;height: 10px;display: inline-block; margin-right: 5px;
+      background: url(http://www.jihui88.com/member/static/images/checkbox-selected.png) no-repeat;
+    }
+    .f-checkout-select.select{
+      background-position: -10px 0;
+    }
+  }
+  // 表单
+  .f-input{
+    width: 260px;
+    box-sizing: border-box;
+    border: 1px solid #d9d9d9;
+    border-radius: 5px;
+    margin: 0 auto;
+    position: relative;
+    input{
+      border-radius: 5px;
+      border:none;
+      width:228px;
+      height: 17px;
+      padding: 10px 15px;
+      outline: 0 none;
+    }
+    .username{
+      border-bottom: 1px solid #d9d9d9;
+      border-bottom-right-radius: 0;
+      border-bottom-left-radius: 0;
+    }
+    .forget-password{
+      position: absolute;
+      color: #ccc;
+      font-size: 12px;
+      right: 15px;
+      margin-top: -26px;
+    }
+  }
+  .submit{
+    background: #ff6700;
+    border-radius: 5px;
+    border: 0 none;
+    cursor: pointer;
+    display: block;
+    width: 260px;
+    margin: 0 auto;
+    text-align: center;
+    height: 38px;
+    color: #fff;
+    font-size: 14px;
+    transition: all 0.3s ease 0s;
+    -moz-transition: all 0.3s ease 0s;
+    -webkit-transition: all 0.3s ease 0s;
+    -o-transition: all 0.3s ease 0s;
+  }
+  .submit:hover {
+  	background:#ff4800;
+  }
+
+  .f-register{
+    .submit{
+      margin-top: 15px;
+    }
+  }
+  .f-message{
+    text-align: left;
+    width: 260px;
+    margin: 0 auto;
+    .tip{
+      font-size: 12px;
+      color: #666;
+      text-align: center;
+      padding-bottom: 10px;
+    }
+    input{
+      border: 1px solid #d9d9d9;
+      padding: 10px 15px;
+      width: 90px;
+      outline: 0 none;
+      margin-top: 10px;
+      border-radius: 3px;
+    }
+    img{
+      vertical-align: middle;
+    }
+    .mobileCode{
+      width: 127px;
+      background: #dedede;
+      color: #999;
+      cursor: pointer;height: 36px;
+    float: right;
+    }
+    .submit{
+      margin-top: 15px;
+    }
+  }
+  .alert {
+    width: 800px;
+    height: 600px;
+    margin: -300px 0 0 -400px;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    background: #fafafa;
+    z-index: 999;
+    border-radius: 3px;
+    box-shadow: 1px 1px 3px #aaa;
+    .head{
+      text-align: center;
+      font-size: 12px;
+      height: 39px;
+      line-height: 39px;
+      border-top-left-radius: 3px;
+      border-top-right-radius: 3px;
+      background: linear-gradient(#fff, #ddd);
+    }
+    .container{
+      width: 786px;
+      height: 554px;
+      margin: 1px auto 0 auto;
+      border: 2px solid #a5a5a5;
+      background: #fff;
+      #wxlogin_container{
+        text-align: center;
+      }
+      iframe{
+        width: 100%;height: 554px;border: none;
+      }
+    }
+  }
+</style>
